8b85cd413504e6661e963852ab617b488ad80db2
SheepNDW <hank3681@gmail.com>
2022-02-05 18:04:00 +0800

    week4 初稿

--- week4/js/product.js
+++ week4/js/product.js
@@ -1,22 +1,27 @@
 import { createApp, ref, onMounted } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js'
 
+// 匯入元件
+import pagination from './components/pagination.js'
+
+let productModal = null
+
 const app = createApp({
+  components: { pagination },
   setup() {
     // api
     const baseURL = 'https://vue3-course-api.hexschool.io/v2'
     const path = 'sheep-api'
 
-    // modal元件
-    let myModal = null
-
     // 產品列表
     const products = ref(null)
 
     // 取得產品資料方法
-    const getProductData = async () => {
+    const pagination = ref(null)
+    const getProductData = async (page = 1) => {
       try {
-        const { data } = await axios.get(`${baseURL}/api/${path}/admin/products`)
+        const { data } = await axios.get(`${baseURL}/api/${path}/admin/products?page=${page}`)
         products.value = data.products
+        pagination.value = data.pagination
       }
       catch (err) {
         alert(err.data.message)
@@ -41,73 +46,27 @@ const app = createApp({
       // 取得 Token
       const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1")
       axios.defaults.headers.common['Authorization'] = token
-
       // 驗證是否為登入狀態
       checkLogin()
-
-      // 獲取model元素
-      myModal = new bootstrap.Modal(document.querySelector('#productModal'))
     })
 
 
     // 當前選中/將要新增的產品
     let selectedProduct = ref({})
     const isEdit = ref(false)
-    const openModel = (status, product = {}) => {
+    const openModal = (status, product = { imagesUrl: [] }) => {
       if (status === 'edit') {
         isEdit.value = true
-        selectedProduct.value = product
+        selectedProduct.value = { ...product }
       } else {
         isEdit.value = false
         selectedProduct.value = product
         // 預設為啟動狀態
         selectedProduct.value.is_enabled = 1
       }
-      myModal.show()
-    }
-    // 新增圖片 
-    const addImg = () => {
-      selectedProduct.value.imagesUrl = []
-      selectedProduct.value.imagesUrl.push('')
+      productModal.show()
     }
 
-    // 關閉產品Modal
-    const closeProductModal = () => {
-      // 清空暫存
-      selectedProduct.value = {}
-      // 重新渲染列表
-      getProductData()
-      myModal.hide()
-    }
-
-    // --- 修改 / 新增 產品的方法 ---
-    const updateProduct = () => {
-      // 新增api
-      let api = `${baseURL}/api/${path}/admin/product`
-      let http = 'post'
-      // 修改api
-      if (isEdit.value) {
-        api = `${baseURL}/api/${path}/admin/product/${selectedProduct.value.id}`
-        http = 'put'
-      }
-      // 發出請求
-      axios[http](api, { data: selectedProduct.value }).then((res) => {
-        Swal.fire({
-          title: res.data.message,
-          icon: 'success',
-          showConfirmButton: false,
-          timer: 1000
-        })
-        // 關閉modal
-        closeProductModal()
-      }).catch(err => {
-        Swal.fire({
-          title: err.data.message,
-          icon: 'error',
-          confirmButtonText: '確定',
-        })
-      })
-    }
 
     // --- 刪除產品的方法 ---
     const removeProduct = (id) => {
@@ -155,22 +114,99 @@ const app = createApp({
     const changeEnabled = (item) => {
       isEdit.value = true
       selectedProduct.value = item
-      updateProduct()
-      // const { data } = await axios.put(`${url}/api/${path}/admin/product/${item.id}`, { data: item })
-      // getProductData()
+
+      let api = `${baseURL}/api/${path}/admin/product/${selectedProduct.value.id}`
+      axios.put(api, { data: selectedProduct.value }).then((res) => {
+        Swal.fire({
+          title: res.data.message,
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1000
+        })
+        // 重新渲染列表
+        getProductData()
+      })
     }
 
     return {
       products,
+      getProductData,
       selectedProduct,
-      openModel,
+      openModal,
       removeProduct,
-      closeProductModal,
       logout,
       isEdit,
-      addImg,
-      updateProduct,
-      changeEnabled
+      changeEnabled,
+      pagination,
     }
   }
-}).mount('#app')
+})
+
+app.component('modal-component', {
+  template: '#templateForProductModal',
+  props: {
+    product: {
+      type: Object,
+      default: () => ({})
+    },
+    isEdit: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    // api
+    const baseURL = 'https://vue3-course-api.hexschool.io/v2'
+    const path = 'sheep-api'
+
+    onMounted(() => {
+      // 獲取modal元素
+      productModal = new bootstrap.Modal(document.querySelector('#productModal'))
+    })
+
+    // 新增圖片 
+    const addImg = () => {
+      props.product.imagesUrl = []
+      props.product.imagesUrl.push('')
+    }
+    // 關閉產品Modal
+    const closeProductModal = () => {
+      productModal.hide()
+    }
+
+    // --- 修改 / 新增 產品的方法 ---
+    const updateProduct = () => {
+      // 新增api
+      let api = `${baseURL}/api/${path}/admin/product`
+      let http = 'post'
+      // 修改api
+      if (props.isEdit) {
+        api = `${baseURL}/api/${path}/admin/product/${props.product.id}`
+        http = 'put'
+      }
+      // 發出請求 (注意要包在"data"裡)
+      axios[http](api, { data: props.product }).then((res) => {
+        Swal.fire({
+          title: res.data.message,
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1000
+        })
+        // 重新渲染列表
+        emit('get-products')
+        // 關閉modal
+        closeProductModal()
+      }).catch(err => {
+        Swal.fire({
+          title: err.data.message,
+          icon: 'error',
+          confirmButtonText: '確定',
+        })
+      })
+    }
+
+    return { addImg, closeProductModal, updateProduct }
+  }
+})
+
+app.mount('#app')
