955615f6945cbbd61e52aa337d8d848d741f24c2
SheepNDW <hank3681@gmail.com>
2022-02-21 19:19:40 +0800

    購物車畫面渲染

--- week6/src/store/modules/cart.js
+++ week6/src/store/modules/cart.js
@@ -1,10 +1,43 @@
 // 購物車模組
 
+import { getCartList, insertCart } from '@/api/cart'
+
 export default {
   namespaced: true,
   state() {
     return {
-      list: []
+      cartData: {
+        carts: [],
+        final_total: 0,
+        total: 0
+      }
+    }
+  },
+  mutations: {
+    SET_CART(state, payload) {
+      state.cartData = payload
+    }
+  },
+  actions: {
+    // 加入購物車
+    addToCart(ctx, payload) {
+      return new Promise((resolve, reject) => {
+        insertCart(payload.id, payload.count).then(() => {
+          return getCartList()
+        }).then((res) => {
+          ctx.commit('SET_CART', res.data)
+          resolve()
+        })
+      })
+    },
+    // 獲取購物車內容
+    findCart(ctx) {
+      return new Promise((resolve, reject) => {
+        getCartList().then((res) => {
+          ctx.commit('SET_CART', res.data)
+          resolve()
+        })
+      })
     }
   }
 }
--- week6/src/views/front/cart/Index.vue
+++ week6/src/views/front/cart/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container mt-3">
+    <Loading :active="isLoading" />
     <!-- 購物車列表 -->
     <div class="text-end">
       <button class="btn btn-outline-danger" type="button">清空購物車</button>
@@ -14,44 +15,54 @@
         </tr>
       </thead>
       <tbody>
-        <template>
-          <tr>
+        <template v-if="$store.state.cart.cartData.carts">
+          <tr v-for="item in $store.state.cart.cartData.carts" :key="item.id">
             <td>
               <button type="button" class="btn btn-outline-danger btn-sm">
                 x
               </button>
             </td>
             <td>
-              {{}}
-              <div class="text-success">已套用優惠券</div>
+              {{ item.product.title }}
             </td>
             <td>
               <div class="input-group input-group-sm">
                 <div class="input-group mb-3">
                   <select id="" class="form-select">
-                    <option>{{}}</option>
+                    <option
+                      :value="num"
+                      v-for="num in 20"
+                      :key="`${num}${item.id}`"
+                    >
+                      {{ num }}
+                    </option>
                   </select>
-                  <span class="input-group-text" id="basic-addon2"> {{}} </span>
+                  <span class="input-group-text" id="basic-addon2">
+                    {{ item.product.unit }}
+                  </span>
                 </div>
               </div>
             </td>
             <td class="text-end">
               <!-- <small class="text-success">折扣價：</small> -->
-              NT${{}}
+              NT${{ item.total }}
             </td>
           </tr>
         </template>
       </tbody>
-      <tfoot>
+      <tfoot v-if="$store.state.cart.cartData.carts">
         <tr>
           <td colspan="3" class="text-end">總計</td>
-          <td class="text-end">{{}}</td>
+          <td class="text-end">{{ $store.state.cart.cartData.total }}</td>
         </tr>
         <tr>
           <td colspan="3" class="text-end text-success">折扣價</td>
-          <td class="text-end text-success">{{}}</td>
+          <td class="text-end text-success">
+            {{ $store.state.cart.cartData.final_total }}
+          </td>
         </tr>
       </tfoot>
+      <CartNone v-else />
     </table>
     <div class="my-5 row justify-content-center">
       <CheckoutForm />
@@ -61,8 +72,21 @@
 
 <script>
 import CheckoutForm from './components/CheckoutForm.vue'
+import CartNone from './components/CartNone.vue'
+import { useStore } from 'vuex'
+import { ref } from 'vue'
 export default {
   name: 'Cart',
-  components: { CheckoutForm }
+  components: { CheckoutForm, CartNone },
+  setup() {
+    const isLoading = ref(true)
+    // 獲取購物車清單
+    const store = useStore()
+    store.dispatch('cart/findCart').then(() => {
+      isLoading.value = false
+    })
+
+    return { isLoading }
+  }
 }
 </script>
