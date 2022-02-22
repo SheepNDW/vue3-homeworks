15883023b107616ea227544db7519a60cb01c8bd
SheepNDW <hank3681@gmail.com>
2022-02-22 14:52:28 +0800

    完成登入頁

---
+++ week6/src/api/login.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+// const path = 'sheep-api'
+
+/**
+ * 登入API
+ * @param {Object} userInfo - 登入表單(username, password)
+ * @returns Promise
+ */
+export const signin = (userInfo) => {
+  return request('/admin/signin', 'post', userInfo)
+}
