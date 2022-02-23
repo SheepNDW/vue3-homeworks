1205e3941d1064c940adc3a33c45287d066e1ed8
SheepNDW <hank3681@gmail.com>
2022-02-23 13:58:36 +0800

    完成後台訂單列表頁展示

--- week6/src/api/order.js
+++ week6/src/api/order.js
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 const path = 'sheep-api'
 
+// 前台 API
 /**
  * 建立訂單
  * @param {Object} order - 訂單資料表單
@@ -8,4 +9,14 @@ const path = 'sheep-api'
  */
 export const createOrder = (order) => {
   return request(`/api/${path}/order`, 'post', { data: order })
+}
+
+// 後台 API
+/**
+ * 取得後台訂單列表
+ * @param {Integer} page - 當前頁碼
+ * @returns Promise
+ */
+export const getOrdersList = (page = 1) => {
+  return request(`/api/${path}/admin/orders?page=${page}`, 'get')
 }
--- week6/src/views/dashboard/orders/AdminOrders.vue
+++ week6/src/views/dashboard/orders/AdminOrders.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>後台訂單管理頁</div>
+  <table class="table align-middle">
+    <thead>
+      <tr>
+        <th scope="col">建立時間</th>
+        <th scope="col">訂單編號</th>
+        <th scope="col">金額</th>
+        <th scope="col">查看詳情</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="order in orders" :key="order.id">
+        <th scope="row">
+          {{ dayjs.unix(order.create_at).format('YYYY-MM-DD HH:mm:ss') }}
+        </th>
+        <td>{{ order.id }}</td>
+        <td>NT${{ order.total }}</td>
+        <td><button type="button" class="btn btn-primary">查看</button></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import { getOrdersList } from '@/api/order'
+import { ref } from 'vue'
+import dayjs from 'dayjs'
 export default {
-  name: 'AdminOrders'
+  name: 'AdminOrders',
+  setup() {
+    // 取得後台訂單列表
+    const orders = ref(null)
+    getOrdersList().then((data) => {
+      orders.value = data.orders
+    })
+
+    return { orders, dayjs }
+  }
 }
 </script>
