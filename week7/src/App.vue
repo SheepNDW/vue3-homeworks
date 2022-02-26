c25b8ac9491d260afbfb4fae2e5f2b8fcfc66bc2
SheepNDW <hank3681@gmail.com>
2022-02-26 11:29:52 +0800

    測試google圓體字型

--- week7/src/App.vue
+++ week7/src/App.vue
@@ -4,4 +4,8 @@
 
 <style lang="scss">
 @import '~bootstrap/scss/bootstrap';
+@import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);
+* {
+  font-family: 'cwTeXYen', sans-serif;
+}
 </style>
